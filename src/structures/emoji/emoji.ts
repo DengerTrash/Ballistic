e0af564664bc9d4c738bec37edc41ba1a403ad9e
ねこでん <59917387+nekodendislike@users.noreply.github.com>
2025-02-26 04:56:08 +0000

    emoji

---
+++ src/structures/emoji/emoji.ts
@@ -0,0 +1,20 @@
+import { User } from "../mod.ts";
+
+export interface EmojiStructure {
+    id: string;
+    name: string;
+    roles?: (any)[];
+    user?: User;
+    require_colons?: boolean;
+    managed?: boolean;
+    animated?: boolean;
+    avivavle?: boolean;
+};
+export class Emoji implements EmojiStructure{
+    readonly id: string;
+    name: string;;
+    constructor(id: string, name: string){
+        this.id = id;
+        this.name = name;
+    }
+}
