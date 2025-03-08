abc5ea67d695ae4b3b76a9f0c8460ff0e7e05e51
ねこでん <59917387+nekodendislike@users.noreply.github.com>
2025-03-08 08:45:14 +0000

    bug

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,6 @@
 /**client */
 export * from './client/mod.ts'
 /**command type */
-export { CommandPayload } from './structures/command/Command.ts'
+export type { CommandPayload } from './structures/command/Command.ts'
 /**intent */
 export * from './intents/intents.ts'
